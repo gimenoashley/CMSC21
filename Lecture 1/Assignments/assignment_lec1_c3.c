4988d5518100598dfd6277cced7ecbc02c4a3066
AJ <90760739+gimenoashley@users.noreply.github.com>
2022-03-06 14:02:41 +0800

    Add files via upload

---
+++ Lecture 1/Assignments/assignment_lec1_c3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (void){
+
+	//declare data type and variable names
+	int value1 = 87, value2 = 15, difference;
+
+	//subtraction formula
+	difference = value1 - value2;
+
+	//display result
+	printf("The difference of %d and %d is %d.", value1, value2, difference);
+
+	return 0;
+}
