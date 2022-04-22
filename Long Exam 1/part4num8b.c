05fd55e1b6d7ace287b5f8e127fc2c8190fa9f3b
AJ <90760739+gimenoashley@users.noreply.github.com>
2022-04-22 14:14:25 +0800

    Add files via upload

---
+++ Long Exam 1/part4num8b.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (void){
+    int a = 2, b = 3;
+    if (b == 3 && a == 2){
+    printf("*****");
+    }
+    else{
+    printf( "-----" );
+    printf( ">>>>>" );
+    printf( "<<<<<" );
+    }
+}
+
---
+++ Long Exam 1/part4num8c.c
@@ -0,0 +1,14 @@
+//display statements
+#include<stdio.h>
+int main (void){
+    int a = 2, b = 3;
+    if (b == 3 && a == 2){
+    printf( "*****" );
+    printf( "<<<<<" );
+    }
+    else{
+    printf("-----");
+    printf( ">>>>>" );
+    }
+}
+
